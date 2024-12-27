16ed40019679830a3e2312674dc8032e2e95b890
dhruv15803 <Dhruvshetty960@gmail.com>
2024-12-27 17:14:02 +0530

    logger

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,8 @@ export const API_URL=import.meta.env.VITE_API_URL || "https://go-feedback-app.on
 
 function App() {
 
+  console.log(import.meta.env.VITE_API_URL);
+  
   return (
     <>
     <AppContextProvider>
